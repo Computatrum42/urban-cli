22914220888b7a210f86ddd5f236b7d22e715084
Computatrum42 <ergunsarigul29a@outlook.com>
2025-04-21 01:45:56 +0300

    The basic program has written

--- Main.rb
+++ Main.rb
@@ -1,8 +1,26 @@
+require 'net/http'
+require 'json'
+require 'uri'
+
+puts "Enter a word:"
+word = gets
+
+url_data = URI("https://api.urbandictionary.com/v0/define?term=" + word)
+response = Net::HTTP.get(url_data)
+data = JSON.parse(response)
+datas = data["list"].first
+
+puts "Word: #{datas['word']}"
+puts ""
+puts "Definition: #{datas['definition']}"
+
+
+
 def main
   puts "testing, wip".reverse
   return
 end
 
-3.times do main() end
+# 3.times do main() end
 
 # Work in progress!
