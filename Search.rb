9629fdb4c065e90738a25c04c112f6b3ffbe7780
Computatrum42 <ergunsarigul29a@outlook.com>
2025-04-25 04:43:12 +0300

    'Entry not found' error added rather than using the Ruby interpreter error.

--- Search.rb
+++ Search.rb
@@ -7,17 +7,21 @@ require 'optparse'
 # Same with Random.rb and its function but with a parameter.
 
 def search(word)
-  url_data = URI("https://api.urbandictionary.com/v0/define?term=" + word)
-  response = Net::HTTP.get(url_data)
-  data = JSON.parse(response)
-  datalist = data["list"].first
+  begin
+    url_data = URI("https://api.urbandictionary.com/v0/define?term=" + word)
+    response = Net::HTTP.get(url_data)
+    data = JSON.parse(response)
+    datalist = data["list"].first
 
-  # Title and date of thread
-  puts "#{datalist['word']} by #{datalist['author']} - #{datalist['written_on']}"
-  puts "Upvote: #{datalist['thumbs_up']} Downvote: #{datalist['thumbs_down']}"
-  puts "" 
-  # Definition
-  raw_def = "#{datalist['definition']}"
-  clean_def = raw_def.gsub(/\[|\]/, '')
-  puts clean_def
+    # Title and date of thread
+    puts "#{datalist['word']} by #{datalist['author']} - #{datalist['written_on']}"
+    puts "Upvote: #{datalist['thumbs_up']} Downvote: #{datalist['thumbs_down']}"
+    puts "" 
+    # Definition
+    raw_def = "#{datalist['definition']}"
+    clean_def = raw_def.gsub(/\[|\]/, '')
+    puts clean_def
+  rescue NameError
+     puts "Entry not found!"
+  end 
 end
